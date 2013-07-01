e08999219b4fc9de30116afe61339e4bc3e48eeb
Adam K Dean <adamkdean@googlemail.com>
2013-07-01 11:45:25 +0100

    Added rudimentary JS and fixed data attributes

--- js/jquery.categories.js
+++ js/jquery.categories.js
@@ -3,4 +3,9 @@
  * 01/07/2013 10:27
  */
 
- 
+ $('.categories li a').click(function() {  	
+ 	var category = $(this).data('category');
+ 	category = (category == '*') ? category : '.' + category;
+	$('.items div').hide();
+ 	$('.items ' + category).show();
+ });

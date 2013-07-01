81d54c73cd590900059762298ad51bfdbf76c58b
Adam K Dean <adamkdean@googlemail.com>
2013-07-01 14:33:29 +0100

    Removed masonry. Added isotope

--- js/jquery.categories.js
+++ js/jquery.categories.js
@@ -3,9 +3,54 @@
  * 01/07/2013 10:27
  */
 
- $('.categories li a').click(function() {  	
- 	var category = $(this).data('category');
- 	category = (category == '*') ? category : '.' + category;
-	$('.items div').hide();
- 	$('.items ' + category).show();
+ $(function() {
+
+ 	var $container = $('.items');
+
+ 	$container.isotope({
+ 		itemSelector: '.item'
+ 	});
+
+	$('.categories li a').click(function() {
+		var category = $(this).data('category');
+		category = (category == '*') ? category : '.' + category;
+
+		$container.isotope({ filter: category });
+
+		/*$current_items = $('.masonry .item');
+		$container.masonry('remove', $current_items);
+
+		$new_items = $('.masonry ' + category);
+		$container.append($new_items).masonry('appended', $new_items).masonry();*/
+	});
+
+
+
+
+
+
+
+/*	$container.masonry({
+		columnWidth: 156,
+		itemSelector: '.item',
+		isAnimated: true
+	})
+	.imagesLoaded(function() {
+		$container.masonry('reloadItems');		
+	});
+
+	$masonry_items = $('.masonry_items .item');
+	$container.append($masonry_items).masonry('appended', $masonry_items).masonry();
+
+	$('.categories li a').click(function() {
+	 	var category = $(this).data('category');
+	 	category = (category == '*') ? category : '.' + category;
+	 	
+	 	$current_items = $('.masonry .item');
+	 	$container.masonry('remove', $current_items);
+
+	 	$new_items = $('.masonry ' + category);
+	 	$container.append($new_items).masonry('appended', $new_items).masonry();
+	 });
+	*/
  });
